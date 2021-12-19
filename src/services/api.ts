ee2f0d690ce10b8660666a695b49c98eb71de603
Fernando Maia <fermaiasoares@aol.com>
2021-12-19 13:19:03 -0300

    Fluxo de autenticao com Twitch

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const { CLIENT_ID } = process.env;
+
 const api = axios.create({
-  baseURL: 'https://api.twitch.tv/helix'
+  baseURL: 'https://api.twitch.tv/helix',
+  headers: {
+    'Client-ID': CLIENT_ID,
+  }
 })
 
 export { api };
